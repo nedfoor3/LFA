6f2dcb29e7f7ec9386fbed04000f562242dd3875
tonatiuhAR <tonatiuhar@gmail.com>
2017-04-28 15:39:16 -0500

    funcionalidad mejorada jugada a jugada

--- app/src/main/java/mx/lfa/com/rawrstudio/viewHolders/PbPViewHolder.java
+++ app/src/main/java/mx/lfa/com/rawrstudio/viewHolders/PbPViewHolder.java
@@ -2,7 +2,10 @@ package mx.lfa.com.rawrstudio.viewHolders;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
+
+import com.bumptech.glide.load.engine.Resource;
 
 import mx.lfa.com.rawrstudio.R;
 
@@ -12,6 +15,8 @@ import mx.lfa.com.rawrstudio.R;
 
 public class PbPViewHolder extends RecyclerView.ViewHolder {
     View pbpView;
+    TextView pbpOportunidad;
+
 
     public PbPViewHolder(View itemView) {
         super(itemView);
@@ -20,12 +25,38 @@ public class PbPViewHolder extends RecyclerView.ViewHolder {
 
     public void setEquipo(String equipo){
         TextView pbpEquipo = (TextView)pbpView.findViewById(R.id.equipo_play_by_play);
+        if(equipo.equals("DINOS")){
+            pbpEquipo.setText(equipo);
+            pbpEquipo.setTextColor(pbpEquipo.getResources().getColor(R.color.primary_dinos));
+        }
+         else if(equipo.equals("DRIVE DINOS")  || equipo.equals("TOUCH DOWN DINOS") || equipo.equals("FG DINOS")){
+            pbpEquipo.setTextColor(pbpEquipo.getResources().getColor(R.color.blanco));
+            pbpEquipo.setBackgroundColor(pbpEquipo.getResources().getColor(R.color.primary_dinos));
+            pbpEquipo.setWidth(5000);
+            pbpEquipo.setText(equipo);
+            pbpOportunidad = (TextView) pbpView.findViewById(R.id.oportunidad_play_by_play);
+            pbpOportunidad.setVisibility(View.GONE);
+        }
+        else if(equipo.equals("MAYAS")){
+            pbpEquipo.setTextColor(pbpEquipo.getResources().getColor(R.color.primary_mayas));
         pbpEquipo.setText(equipo);
+        }
+        else if(equipo.equals("DRIVE MAYAS") || equipo.equals("TOUCH DOWN MAYAS") || equipo.equals("FG MAYAS")){
+            pbpEquipo.setTextColor(pbpEquipo.getResources().getColor(R.color.blanco));
+            pbpEquipo.setBackgroundColor(pbpEquipo.getResources().getColor(R.color.primary_mayas));
+            pbpEquipo.setWidth(5000);
+            pbpEquipo.setText(equipo);
+            pbpOportunidad = (TextView) pbpView.findViewById(R.id.oportunidad_play_by_play);
+            pbpOportunidad.setVisibility(View.GONE);
+        }
+        else {
+            pbpEquipo.setText(equipo);
+        }
 
     }
 
     public void setOportunidad(String oportunidad) {
-        TextView pbpOportunidad = (TextView) pbpView.findViewById(R.id.oportunidad_play_by_play);
+        pbpOportunidad = (TextView) pbpView.findViewById(R.id.oportunidad_play_by_play);
         pbpOportunidad.setText(oportunidad);
     }
 
